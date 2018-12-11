6ca05cadc07ef8f27c9bbab51a45d205bc798e5e
sadranyi <sadranyi@gmail.com>
2018-12-11 07:00:56 -0800

    added function to generate coupon code

--- functions/GenerateCoupon/index.js
+++ functions/GenerateCoupon/index.js
@@ -1,16 +1,10 @@
-module.exports = async function (context, req) {
-    context.log('JavaScript HTTP trigger function processed a request.');
+var Chance = require('chance')
 
-    if (req.query.name || (req.body && req.body.name)) {
-        context.res = {
-            // status: 200, /* Defaults to 200 */
-            body: "Hello " + (req.query.name || req.body.name)
-        };
+module.exports = async function (context, req) {
+
+    var chance = new Chance()
+    
+    context.res = { // status: 200, /* Defaults to 200 */
+        body: chance.hash({ length: 6, casing: 'upper' })
     }
-    else {
-        context.res = {
-            status: 400,
-            body: "Please pass a name on the query string or in the request body"
-        };
-    }
-};
+}
