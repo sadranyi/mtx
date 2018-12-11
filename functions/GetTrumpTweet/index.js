1ccfd8357774da3ded229616eff50cce33c17cda
sadranyi <sadranyi@gmail.com>
2018-12-11 03:20:09 -0800

    added chance test

--- functions/GetTrumpTweet/index.js
+++ functions/GetTrumpTweet/index.js
@@ -1,14 +1,17 @@
-var chance = require('chance');
+var Chance = require('chance');
 var fake = require('faker');
 var axios = require('axios');
 
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
+    var chance = new Chance();
+    var random_string = chance.string();
+
     if (req.query.name || (req.body && req.body.name)) {
         context.res = {
             // status: 200, /* Defaults to 200 */
-            body: "Hello " + (req.query.name || req.body.name)
+            body: "Hello " + (req.query.name || req.body.name) + " " + random_string
         };
     }
     else {
