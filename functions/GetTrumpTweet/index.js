1beb74e3c3c954e0ce378ad8bef54410328cd119
sadranyi <sadranyi@gmail.com>
2018-12-11 03:38:07 -0800

    added user generator

--- functions/GetTrumpTweet/index.js
+++ functions/GetTrumpTweet/index.js
@@ -1,23 +1,26 @@
-var Chance = require('chance');
-var fake = require('faker');
-var axios = require('axios');
+var Chance = require('chance')
+var fake = require('faker')
+var axios = require('axios')
 
 module.exports = async function (context, req) {
-    context.log('JavaScript HTTP trigger function processed a request.');
+  context.log('JavaScript HTTP trigger function processed a request.')
 
-    var chance = new Chance();
-    var random_string = chance.string();
-
-    if (req.query.name || (req.body && req.body.name)) {
-        context.res = {
-            // status: 200, /* Defaults to 200 */
-            body: "Hello " + (req.query.name || req.body.name) + " " + random_string
+  var chance = new Chance()
+  chance.mixin({
+    'employee': function(){
+        return {
+            first: chance.first(),
+            last: chance.last(),
+            email: chance.email(),
+            gender: chance.gender(),
+            profession: chance.profession(),
+            avatar: chance.avatar()
         };
     }
-    else {
-        context.res = {
-            status: 400,
-            body: "Please pass a name on the query string or in the request body"
-        };
-    }
-};
+  });
+
+    var employeeObject = chance.employee();
+
+  context.res = { // status: 200, /* Defaults to 200 */
+    body: JSON.parse(employeeObject) };
+}
